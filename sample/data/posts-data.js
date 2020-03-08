61dde1a18fbbb2adb970467fcd97a2f86d5f7365
易成杰 <626659321@qq.com>
2020-03-08 22:25:10 +0800

    音乐播放

--- sample/data/posts-data.js
+++ sample/data/posts-data.js
@@ -11,6 +11,11 @@ let postList = [{
     dateTime: '24小时前',
     detail: '今天(3月6日，周五)，全球市场暴动：欧洲股市主要股指重挫，跌幅几乎都超过3%；美国股市期货同样下跌，道指期货跌近400点，隔夜道指收跌近100点。美债收益率狂泻不已，因资金涌入美债避险；美元跌势加剧，最低96.17，欧元/美元大反弹，最高1.1290；美元/日元重挫至105.25；澳元、纽元等货币也在反弹。现货黄金重拾1680关口，最高1685.70美元/盎司，今夜很有可能挑战1700关口。全球新冠肺炎疫情担忧加剧，欧洲确诊病例暴增，日本韩国伊朗的疫情有爆发的可能性。分析认为，市场担忧全球疫情危机，冲击经济前景，导致投资者恐慌性抛售！眼下，美元暴跌的背后，缘于美联储的降息恐将“巨大”，且市场更是押注3月还有一次降息！今天，市场聚焦美国2月非农就业报告，有望引发新一波的行情！如果非农意外恶化，市场恐慌势必加剧',
     postId: 0,
+    music: {
+      url: "http://music.163.com/song/media/outer/url?id=142604.mp3",
+      title: "夜夜夜夜-齐秦",
+      coverImg: "http://y.gtimg.cn/music/photo_new/T002R150x150M000001TEc6V0kjpVC.jpg?max_age=2592000"
+    }
   },
   {
     avatar: '/images/avatar/2.png',
@@ -24,7 +29,12 @@ let postList = [{
     author: '迷的城',
     dateTime: '24小时前',
     detail: '北京时间3月5日，唯品会对外公布第四季度及全年财报。回顾唯品会的2019年，股价表现意外的涨幅很高。2019年美国中概股共有297家企业，其间股价涨幅TOP3的分别为：唯品会、新东方、万国数据，三者涨幅均超过100%，其中唯品会以165.9%居冠。',
-    postId: 1
+    postId: 1,
+    music: {
+      url: "http://music.163.com/song/media/outer/url?id=108220.mp3",
+      title: "鬼迷心窍-李宗盛",
+      coverImg: "http://y.gtimg.cn/music/photo_new/T002R150x150M000002xOmp62kqSic.jpg?max_age=2592000"
+    }
   },
   {
     avatar: '/images/avatar/3.png',
@@ -38,7 +48,12 @@ let postList = [{
     author: '知乎',
     dateTime: '3天前',
     detail: '日前，“北京银行2020年度抗疫主题小微金融债”成功获批，成为全国首单获批的抗疫主题小微金融债。本次债券注册额度为600亿元，其中不少于200亿元将专项用于与新型冠状病毒感染肺炎疫情防控相关的小型微型企业贷款。',
-    postId: 2
+    postId: 2,
+    music: {
+      url: "http://music.163.com/song/media/outer/url?id=27538254.mp3",
+      title: "女儿情-万晓利",
+      coverImg: "http://y.gtimg.cn/music/photo_new/T002R150x150M000004Wv5BO30pPc0.jpg?max_age=2592000"
+    }
   },
   {
     avatar: '/images/avatar/3.png',
@@ -52,7 +67,12 @@ let postList = [{
     author: '猫是猫的猫',
     dateTime: 'Nov 20 2016',
     detail: '今日晚间，14家券商资管子公司2月业绩全部披露完毕。据《每日经济新闻》记者统计，14家券商资管2月合计实现营业收入14.13亿元，环比增长29.20%；净利润合计6.43亿元，环比增长49.47%。',
-    postId: 3
+    postId: 3,
+    music: {
+      url: "http://music.163.com/song/media/outer/url?id=108119.mp3",
+      title: "恋恋风尘-老狼",
+      coverImg: "http://y.gtimg.cn/music/photo_new/T002R150x150M000001VaXQX1Z1Imq.jpg?max_age=2592000",
+    }
   },
   {
     avatar: '/images/avatar/5.png',
@@ -66,7 +86,12 @@ let postList = [{
     author: '深白色',
     dateTime: 'Nov 20 2016',
     detail: '企业复工复产供需对接平台由新华网客户端和央企电商联盟联合推出，国资委科创局、国资委新闻中心指导，国网电商公司提供技术支持。如果你在生产设备、工厂、仓储物流或原材料方面存在缺口或者有供应能力，都可以登录该平台。',
-    postId: 4
+    postId: 4,
+    music: {
+      url: "http://music.163.com/song/media/outer/url?id=188204.mp3",
+      title: "沉默是金-张国荣",
+      coverImg: "http://y.gtimg.cn/music/photo_new/T002R150x150M000003at0mJ2YrR2H.jpg?max_age=2592000"
+    }
   }
 ];
 
--- sample/pages/posts/post-detail/post-detail.js
+++ sample/pages/posts/post-detail/post-detail.js
@@ -8,40 +8,90 @@ Page({
    */
   data: {
     postId: -1,
+    isPlayingMusic: false,
     collected: false
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-    let postId = options.id ;
-    let postData = postsData.postList[postId] ;
-    this.setData({ ...postData }) ;
+  onLoad: function(options) {
+    let postId = options.id;
+    let postData = postsData.postList[postId];
+    this.setData({ ...postData
+    });
 
     //从缓存中获取是否被收藏
-    let postsCollected = wx.getStorageSync('posts_collected') ;
-    if (postsCollected){
+    let postsCollected = wx.getStorageSync('posts_collected');
+    if (postsCollected) {
       let postCollected = postsCollected[postId];
-      this.setData({collected: postCollected}) ;
-    }else{
-      postsCollected = {} ;
-      postsCollected[postId] = false ;
-      wx.setStorageSync('posts_collected', postsCollected) ;
+      this.setData({
+        collected: postCollected
+      });
+    } else {
+      postsCollected = {};
+      postsCollected[postId] = false;
+      wx.setStorageSync('posts_collected', postsCollected);
     }
   },
-  onCollectionTap: function(event){
-    let nextCollected = !this.data.collected ;
+  onCollectionTap: function(event) {
+    //收藏变为未收藏，未收藏变为收藏
+    let nextCollected = !this.data.collected;
     this.setData({
       collected: nextCollected
-    }) ;
+    });
 
     //保存到storage中
     let postsCollected = wx.getStorageSync('posts_collected');
-    postsCollected[this.data.postId] = nextCollected ;
+    postsCollected[this.data.postId] = nextCollected;
     wx.setStorageSync('posts_collected', postsCollected);
+
+    //用户反馈
+    wx.showToast({
+      title: nextCollected ? '收藏成功' : '取消收藏',
+      icon: 'success'
+    })
   },
-  onShareTap: function(event){
-    
+  onShareTap: function(event) {
+    let itemList = [
+      '分享给微信好友',
+      '分享到朋友圈',
+      '分享到QQ',
+      '分享到微博'
+    ];
+    wx.showActionSheet({
+      itemList: itemList,
+      itemColor: '#405f80',
+      success: function(res) {
+        //res.cancel 用户是不是点击了取消
+        //res.tapIndex 数组元素的序号，从0开始
+        if (!res.cancel) {
+          wx.showModal({
+            title: '用户' + itemList[res.tapIndex],
+            content: '现在无法实现分享功能'
+          })
+        }
+      }
+    })
+  },
+  onMusicTap: function(event) {
+    let nextIsPlayingMusic = !this.data.isPlayingMusic;
+    this.setData({
+      isPlayingMusic: nextIsPlayingMusic
+    });
+
+    //音乐播放
+    let postData = postsData.postList[this.data.postId];
+    let musicData = postData.music;
+
+    if (nextIsPlayingMusic) {
+      wx.playBackgroundAudio({
+        dataUrl: musicData.url,
+        title: musicData.title,
+        coverImgUrl: musicData.coverImg
+      })
+    } else {
+      wx.pauseBackgroundAudio();
+    }
   }
 })
