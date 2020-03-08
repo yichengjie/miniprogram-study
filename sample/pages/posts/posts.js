895d8e3fcfd1b0d1fcb3dbb0210c2b48163469c9
易成杰 <626659321@qq.com>
2020-03-08 15:41:17 +0800

    保存

--- sample/pages/posts/posts.js
+++ sample/pages/posts/posts.js
@@ -5,62 +5,32 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    postList:[]
   },
-
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+    let posts = [
+      {
+        author_img: '/images/avatar/1.png',
+        date: 'Sep 18 2016',
+        title: '正是虾肥蟹壮时',
+        post_img: '/images/post/crab.png',
+        content: '世界卫生组织6日公布的最新数据显示，截至欧洲中部时间6日10时（北京时间6日17时）',
+        view_num: '112',
+        collect_num: '96'
+      },
+      {
+        author_img: '/images/avatar/2.png',
+        date: 'Sep 25 2016',
+        title: '比利.林恩的中场故事',
+        post_img: '/images/post/bl.png',
+        content: '3月5日的外交部例行记者会上，外交部新闻发言人首次对印度扣留中国“大翠云”号货轮货物做出了正式回应。',
+        view_num: '112',
+        collect_num: '96'
+      }
+    ] ;
+    this.setData({ posts }) ;
   }
 })
--- sample/pages/welcome/welcome.js
+++ sample/pages/welcome/welcome.js
@@ -1,3 +1,13 @@
 Page({
- 
+  onTap:function(event){
+    // wx.navigateTo({
+    //   url: '../posts/posts',
+    // }) ;
+    wx.redirectTo({
+      url: '../posts/posts',
+    })
+  },
+  onHide: function(){
+    console.info("welcome page is on hide ") ;
+  }
 })
