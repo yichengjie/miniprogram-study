c5d0679f9529b5ae66fa9c1fb9dd7ca8924db8b2
易成杰 <626659321@qq.com>
2020-03-08 16:05:40 +0800

    数据展示开发完成

---
+++ sample/data/posts-data.js
@@ -0,0 +1,51 @@
+let postList = [{
+    avatar: '/images/avatar/1.png',
+    date: 'Sep 18 2016',
+    title: '正是虾肥蟹壮时',
+    imgSrc: '/images/post/crab.png',
+    content: '世界卫生组织6日公布的最新数据显示，截至欧洲中部时间6日10时（北京时间6日17时）',
+    view_num: '112',
+    collect_num: '96'
+  },
+  {
+    avatar: '/images/avatar/2.png',
+    date: 'Sep 25 2016',
+    title: '比利.林恩的中场故事',
+    imgSrc: '/images/post/bl.png',
+    content: '3月5日的外交部例行记者会上，外交部新闻发言人首次对印度扣留中国“大翠云”号货轮货物做出了正式回应。',
+    collection: '96',
+    reading: '112'
+  },
+  {
+    avatar: '/images/avatar/3.png',
+    date: 'Nov 12 2016',
+    title: '当我们在谈论经济学时，我们在谈论什么？',
+    imgSrc: '/images/post/sls.jpg',
+    content: '据AlphaMetal数据显示，自进入2月以来，伦锌现货对三个月期锌维持Contango结构，今日最新报20.5美元/吨。而伦锌在春节期间出现持续集中交仓之后，库存大量增加，3月4日最新库存74725吨，注册仓单70525吨，也使得现货价格持续承压',
+    collection: '62',
+    reading: '92'
+  },
+  {
+    avatar: '/images/avatar/3.png',
+    date: 'Nov 20 2016',
+    title: '从视觉到触觉 这款VR手套能给你真实触感',
+    imgSrc: '/images/post/vr.png',
+    content: '央视网消息（新闻联播）：物流业对统筹做好疫情防控和经济社会发展工作具有重要支撑作用，记者从国务院新闻办今天（3月6日）举行的发布会上了解到，目前，物流业复工有序，速度加快',
+    collection: '26',
+    reading: '102'
+  },
+  {
+    avatar: '/images/avatar/5.png',
+    date: 'Nov 20 2016',
+    title: '微信.小程序开发工具安装指南',
+    imgSrc: '/images/post/xiaolong.jpg',
+    content: '在大盘强势上涨的背景下，皖通科技3月5日股价逆势下跌3.25%，因“董事会内战”而引发的焦虑情绪似已在投资者群体中蔓延。',
+    collection: '92',
+    reading: '102'
+  }
+];
+
+module.exports = {
+  postList: postList
+} ;
+
--- sample/pages/posts/posts.js
+++ sample/pages/posts/posts.js
@@ -1,4 +1,5 @@
-// pages/posts/posts.js
+let posData = require('../../data/posts-data.js') ;
+
 Page({
 
   /**
@@ -11,26 +12,6 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    let posts = [
-      {
-        author_img: '/images/avatar/1.png',
-        date: 'Sep 18 2016',
-        title: '正是虾肥蟹壮时',
-        post_img: '/images/post/crab.png',
-        content: '世界卫生组织6日公布的最新数据显示，截至欧洲中部时间6日10时（北京时间6日17时）',
-        view_num: '112',
-        collect_num: '96'
-      },
-      {
-        author_img: '/images/avatar/2.png',
-        date: 'Sep 25 2016',
-        title: '比利.林恩的中场故事',
-        post_img: '/images/post/bl.png',
-        content: '3月5日的外交部例行记者会上，外交部新闻发言人首次对印度扣留中国“大翠云”号货轮货物做出了正式回应。',
-        view_num: '112',
-        collect_num: '96'
-      }
-    ] ;
-    this.setData({ posts }) ;
+    this.setData({ posts: posData.postList }) ;
   }
 })
