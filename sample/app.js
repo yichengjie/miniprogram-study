f6551eb58d0a0baeaad036b66b2c206683c5af43
易成杰 <626659321@qq.com>
2020-03-09 21:20:25 +0800

    电影静态页面开发

--- sample/app.js
+++ sample/app.js
@@ -2,6 +2,7 @@ App({
   globalData:{
     g_isPlayingMusic: false,
     g_currentMusicPostId: null,
-    doubanBase: 'http://t.yushu.im'
+    //doubanBase: 'http://t.yushu.im'
+    doubanBase: 'https://douban.uieee.com'
   }
 })
