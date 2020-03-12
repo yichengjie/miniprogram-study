9cd30c2a1baa30465f2bd05c64c595a6fea8e833
易成杰 <626659321@qq.com>
2020-03-12 17:19:52 +0800

    保存

--- helloworld/pages/index/index.js
+++ helloworld/pages/index/index.js
@@ -1,54 +1,29 @@
-//index.js
-//获取应用实例
-const app = getApp()
-
 Page({
-  data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
-  },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
-  },
-  onLoad: function () {
-    if (app.globalData.userInfo) {
+   data: {
+      options: [{
+         city_id: '001',
+         city_name: '北京'
+      }, {
+         city_id: '002',
+         city_name: '上海'
+      }, {
+         city_id: '003',
+         city_name: '深圳'
+      }],
+      selected: {}
+   },
+   change(e) {
       this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
+         selected: { ...e.detail }
       })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
+      wx.showToast({
+         title: `${this.data.selected.id} - ${this.data.selected.name}`,
+         icon: 'success',
+         duration: 1000
       })
-    }
-  },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
-    })
-  }
+   },
+   close() {
+      // 关闭select
+      this.selectComponent('#select').close()
+   }
 })
