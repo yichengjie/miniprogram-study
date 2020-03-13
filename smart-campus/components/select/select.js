3f7f9725cd91831ac34e912e70c26d427c09a61c
易成杰 <626659321@qq.com>
2020-03-13 22:50:05 +0800

    保存

---
+++ smart-campus/components/select/select.js
@@ -0,0 +1,101 @@
+Component({
+   properties: {
+      options: {
+         type: Array,
+         value: []
+      },
+      defaultOption: {
+         type: Object,
+         value: {
+            id: '',
+            name: '请选择'
+         }
+      },
+      key: {
+         type: String,
+         value: 'id'
+      },
+      text: {
+         type: String,
+         value: 'name'
+      },
+      value: {
+         type: String,
+         value: '' 
+      }
+   },
+   data: {
+      result: [],
+      isShow: false,
+      current: {}
+   },
+   methods: {
+      optionTap(e) {
+         let dataset = e.target.dataset
+         this.setData({
+            current: dataset,
+            isShow: false
+         });
+         // 调用父组件方法，并传参
+         this.triggerEvent("change", { ...dataset })
+      },
+      openClose() {
+         this.setData({
+            isShow: !this.data.isShow
+         })
+      },
+      // 此方法供父组件调用
+      close() {
+         this.setData({
+            isShow: false
+         })
+      }
+   },
+   lifetimes: {
+      attached() {
+         // 属性名称转换, 如果不是 { id: '', name:'' } 格式，则转为 { id: '', name:'' } 格式
+         let result = parseSelectResult(this.data.options, this.data.key, this.data.text) ;
+         this.setData({
+            current: Object.assign({}, this.data.defaultOption),
+            result: result
+         }) ;
+      }
+   },
+   observers: {
+      'options': function (options) {
+         let result = parseSelectResult(options, this.data.key, this.data.text);
+         this.setData({
+            result: result
+         }) ;
+      },
+      'value': function (newValue){
+         let newItem  = null ;
+         for (let item of this.data.options){
+            if((item[this.data.key] +'') == (newValue +'')){
+               newItem = item ;
+               break ;
+            }
+         } 
+         if (newItem != null){
+            let {[this.data.key]: id, [this.data.text]:name} = newItem ;
+            this.setData({
+               current: { id, name } 
+            });
+         }
+      }
+   }
+});
+
+/**
+ * 解析select组件的result数据
+ */
+function parseSelectResult(options, key, text) {
+   let result = []
+   if (key !== 'id' || text !== 'name') {
+      for (let item of options) {
+         let { [key]: id, [text]: name } = item;
+         result.push({ id, name })
+      }
+   }
+   return result;
+}
